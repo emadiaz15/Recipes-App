938a6d8d3f9280a33856dc11213e4323922577c9
emadiaz15 <e.diaz@itecriocuarto.org.ar>
2024-09-03 21:16:49 -0300

    fix error localstore

--- src/context/RecipeContext.jsx
+++ src/context/RecipeContext.jsx
@@ -2,22 +2,23 @@ import React, { createContext, useState, useEffect } from 'react'; // Importa Re
 
 const RecipeContext = createContext(); // Crea un nuevo contexto llamado RecipeContext.
 
-const RecipeProvider = ({ children }) => { // Define un componente RecipeProvider que envolverá a otros componentes para proporcionarles el contexto de recetas.
-  const [recipes, setRecipes] = useState([]); // Declara un estado 'recipes' para almacenar la lista de recetas, inicialmente vacío.
-
-  // Carga las recetas desde el localStorage cuando el componente se monta por primera vez.
-  useEffect(() => {
-    const savedRecipes = JSON.parse(localStorage.getItem('recipes')) || []; // Intenta cargar recetas guardadas desde localStorage, o un array vacío si no hay recetas.
-    setRecipes(savedRecipes); // Actualiza el estado 'recipes' con las recetas cargadas desde localStorage.
-  }, []); // La lista de dependencias vacía asegura que este efecto se ejecute solo una vez al montar el componente.
+const RecipeProvider = ({ children }) => { 
+  // Define un componente RecipeProvider que envolverá a otros componentes para proporcionarles el contexto de recetas.
+  
+  // Inicializa el estado 'recipes' cargando las recetas desde localStorage, si existen.
+  const [recipes, setRecipes] = useState(() => {
+    const savedRecipes = localStorage.getItem('recipes');
+    return savedRecipes ? JSON.parse(savedRecipes) : [];
+  });
 
   // Guarda las recetas en localStorage cada vez que cambia el estado 'recipes'.
   useEffect(() => {
-    localStorage.setItem('recipes', JSON.stringify(recipes)); // Convierte el estado 'recipes' a una cadena JSON y lo guarda en localStorage.
+    localStorage.setItem('recipes', JSON.stringify(recipes)); 
+    // Convierte el estado 'recipes' a una cadena JSON y lo guarda en localStorage.
   }, [recipes]); // Este efecto se ejecuta cada vez que 'recipes' cambia.
 
   return (
-    <RecipeContext.Provider value={{ recipes, setRecipes }}> 
+    <RecipeContext.Provider value={{ recipes, setRecipes }}>
       {/* Proporciona el contexto de recetas a los componentes hijos */}
       {children} 
       {/* Renderiza todos los componentes hijos que están envueltos por RecipeProvider */}
@@ -25,4 +26,5 @@ const RecipeProvider = ({ children }) => { // Define un componente RecipeProvide
   );
 };
 
-export { RecipeProvider, RecipeContext }; // Exporta RecipeProvider y RecipeContext para que puedan ser utilizados en otras partes de la aplicación.
+export { RecipeProvider, RecipeContext }; 
+// Exporta RecipeProvider y RecipeContext para que puedan ser utilizados en otras partes de la aplicación.
